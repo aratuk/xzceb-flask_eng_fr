c1689d7f000be02d37df2bfb8ff6a6270806a676
aratuk <ian.wentzell@gmail.com>
2023-04-05 11:44:52 -0400

    rebase

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -9,18 +9,18 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-authenticator = IAMAuthenticator('apikey')
+authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('url')
+language_translator.set_service_url(url)
 
 def englishToFrench(englishText):
-    frenchText = language_translator.translate(text='englishText', model_id='en-fr').get_result()
+    frenchText = language_translator.translate(text=englishText, model_id='en-fr').get_result()
     return frenchText
 
 def frenchToEnglish(frenchText):
-    englishText = language_translator.translate(text='frenchText', model_id='fr-en').get_result()
+    englishText = language_translator.translate(text=frenchText, model_id='fr-en').get_result()
     return englishText
