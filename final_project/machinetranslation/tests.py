79719628faafa53dc69ee4914c77b26d0f556ea0
aratuk <ian.wentzell@gmail.com>
2023-04-05 16:05:31 -0400

    commented and working translator & tests

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,15 +1,24 @@
 import unittest
 from translator import englishToFrench, frenchToEnglish
 
-class test_translator(unittest.TestCase):
-    #def test_e2f_null(self):
-    #    self.assertIsNotNone(self,'')
-    #def test_f2e_null(self):
-    #    self.assertIsNotNone(self,'')
+#English to French
+class test_e2f_translator(unittest.TestCase):
+    #Test that Hello = Bonjour
     def test_e2f_hi(self):
-        self.assertEqual(englishToFrench('Hello'), 'Bonjour')
+        self.assertEqual(englishToFrench('Hello')['translations'][0]['translation'], 'Bonjour')
+    #Test null
+    def test_e2f_null(self):
+        self.assertNotEqual(englishToFrench('Null'), '')
+    
+#French to English
+class test_f2e_translator(unittest.TestCase):
+    #Test that Bonjour = Hello
     def test_f2e_hi(self):
-        self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
+        self.assertEqual(frenchToEnglish('Bonjour')['translations'][0]['translation'], 'Hello')
+    #Test null
+    def test_f2e_null(self):
+        self.assertNotEqual(frenchToEnglish('Null'), '')
+
 
 if __name__ == "__main__":
     unittest.main()    
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,8 @@
+'''Invoke Watson API to facilitate English <> French translation'''
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,6 +10,9 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
+'''
+Instantiate Watson
+'''
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
@@ -17,10 +21,12 @@ language_translator = LanguageTranslatorV3(
 
 language_translator.set_service_url(url)
 
-def englishToFrench(englishText):
-    frenchText = language_translator.translate(text=englishText, model_id='en-fr').get_result()
-    return frenchText
+def english_to_french(english_text):
+    '''English to French'''
+    french_text = language_translator.translate(text=english_text, model_id='en-fr').get_result()
+    return french_text
 
-def frenchToEnglish(frenchText):
-    englishText = language_translator.translate(text=frenchText, model_id='fr-en').get_result()
-    return englishText
+def french_to_english(french_text):
+    '''French to English'''
+    english_text = language_translator.translate(text=french_text, model_id='fr-en').get_result()
+    return english_text
