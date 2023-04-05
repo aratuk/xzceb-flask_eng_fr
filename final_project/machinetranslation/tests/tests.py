3aecdc2d60c898b0f6b967a2e521d89d8623070a
aratuk <ian.wentzell@gmail.com>
2023-04-05 18:18:59 -0400

    Final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,23 +1,23 @@
 import unittest
-from translator import englishToFrench, frenchToEnglish
+from .translator import english_to_french, french_to_english
 
 #English to French
 class test_e2f_translator(unittest.TestCase):
     #Test that Hello = Bonjour
     def test_e2f_hi(self):
-        self.assertEqual(englishToFrench('Hello')['translations'][0]['translation'], 'Bonjour')
+        self.assertEqual(english_to_french('Hello')['translations'][0]['translation'], 'Bonjour')
     #Test null
     def test_e2f_null(self):
-        self.assertNotEqual(englishToFrench('Null'), '')
+        self.assertNotEqual(english_to_french('Null'), '')
     
 #French to English
 class test_f2e_translator(unittest.TestCase):
     #Test that Bonjour = Hello
     def test_f2e_hi(self):
-        self.assertEqual(frenchToEnglish('Bonjour')['translations'][0]['translation'], 'Hello')
+        self.assertEqual(french_to_english('Bonjour')['translations'][0]['translation'], 'Hello')
     #Test null
     def test_f2e_null(self):
-        self.assertNotEqual(frenchToEnglish('Null'), '')
+        self.assertNotEqual(french_to_english('Null'), '')
 
 
 if __name__ == "__main__":
